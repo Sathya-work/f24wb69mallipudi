7a443a4161edacf2b74a3f452a54ef0d24fd1ddc
Sathya-work <sathyanarayana.work@gmail.com>
2024-11-06 13:45:41 -0600

    Added grid

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var inventionsRouter = require('./routes/inventions');
+var gridRouter = require('./routes/grid')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/inventions', inventionsRouter);
+app.use('/grid', gridRouter); 
 
 app.use(function(req, res, next) {
   next(createError(404));
